f604b91007a80038867030f0e86a9f808668e3fa
David Sugar <dyfet@gnutelephony.org>
2007-07-31 23:40:29 +0000

    version info

--- server/server.cpp
+++ server/server.cpp
@@ -324,6 +324,15 @@ extern "C" int main(int argc, char **argv)
 			continue;
 		} 
 
+		if(!stricmp(*argv, "-version")) {
+			printf("SIP Witch " VERSION "\n"
+				"Copyright (C) 2007 David Sugar, Tycho Softworks\n"
+				"License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
+				"This is free software: you are free to change and redistribute it.\n"
+				"There is NO WARRANTY, to the extent permitted by law.\n");
+			exit(0);
+		}
+
 		if(!strcmp(*argv, "-u") || !stricmp(*argv, "-user")) {
 			user = *(++argv);
 			if(!user) {
