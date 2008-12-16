d009cba493cddeef2295769de120d4523ec012db
David Sugar <dyfet@gnutelephony.org>
2008-12-16 18:45:54 +0000

    updated copyright notices

--- server/forward.cpp
+++ server/forward.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2006-2007 David Sugar, Tycho Softworks.
+// Copyright (C) 2008 David Sugar, Tycho Softworks.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
