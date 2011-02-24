a9baf58df1bb75df295569336f0f372d294d392b
David Sugar <dyfet@gnutelephony.org>
2011-02-24 17:08:42 +0000

    preliminary cmake

---
+++ config.h.cmake
@@ -0,0 +1,28 @@
+#define PACKAGE "${PROJECT_NAME}"
+#define STDC_HEADERS 1
+#define VERSION "${VERSION}"
+
+#define DEFAULT_LIBPATH "${DEFAULT_LIBPATH}"
+#define DEFAULT_LIBEXEC "${DEFAULT_LIBEXEC}"
+#define DEFAULT_VARPATH "${DEFAULT_VARPATH}"
+#define DEFAULT_CFGPATH "${DEFAULT_CFGPATH}"
+
+#cmakedefine HAVE_GETHOSTNAME 1
+#cmakedefine HAVE_ATEXIT 1
+#cmakedefine HAVE_GETUID 1
+#cmakedefine HAVE_IOCTL_H 1
+#cmakedefine HAVE_MKFIFO 1
+#cmakedefine HAVE_NET_IF_H 1
+#cmakedefine HAVE_PWD_H 1
+#cmakedefine HAVE_SETPGRP 1
+#cmakedefine HAVE_SETRLIMIT 1
+#cmakedefine HAVE_SIGWAIT 1
+#cmakedefine HAVE_SIGWAIT2 1
+#cmakedefine HAVE_SYMLINK 1
+#cmakedefine HAVE_SYSLOG_H 1
+#cmakedefine HAVE_SYS_RESOURCE_H 1
+#cmakedefine HAVE_SYS_SOCKIO_H 1
+#cmakedefine HAVE_SYS_STAT_H 1
+#cmakedefine HAVE_TLS 1
+
+
