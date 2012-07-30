50ef250e84ef532d7a0297dcee6c5d2881d22c05
David Sugar <dyfet@gnutelephony.org>
2012-07-30 12:54:16 -0400

    config changed for better out of source builds

--- utils/sipquery.cpp
+++ utils/sipquery.cpp
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "../config.h"
+#include <sipwitch-config.h>
 #include <eXosip2/eXosip.h>
 #include <ucommon/ucommon.h>
 
--- utils/sipwitch.cpp
+++ utils/sipwitch.cpp
@@ -13,9 +13,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "../config.h"
-#include "sipwitch/sipwitch.h"
-#include "ucommon/secure.h"
+#include <sipwitch-config.h>
+#include <sipwitch/sipwitch.h>
+#include <ucommon/secure.h>
 #ifdef _MSWINDOWS_
 #include <windows.h>
 #include <io.h>
