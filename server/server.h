63be569a4462b1c508c1a4e321768b99a83f1ed6
David Sugar <dyfet@gnutelephony.org>
2012-08-07 09:13:01 -0400

    prep for release

--- server/server.h
+++ server/server.h
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#undef  HAVE_CONFIG_H
+
 #include <sipwitch-config.h>
 #include <sipwitch/sipwitch.h>
 #include <ucommon/secure.h>
