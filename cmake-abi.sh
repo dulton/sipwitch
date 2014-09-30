9e8a2bab8c29a329b2aa8570f2e6df00cf566433
David Sugar <dyfet@gnutelephony.org>
2014-09-30 04:40:12 -0400

    modernized and simplified cmake

--- cmake-abi.sh
+++ cmake-abi.sh
@@ -3,4 +3,10 @@ ltver=`grep LT_VERSION= configure.ac | sed -e "s/^.*=//" | sed -e "s/\"//g"`
 release=`echo $ltver | sed -e "s/:.*//"`
 compat=`echo $ltver | sed -e "s/.*://"`
 abi=`echo $ltver | sed -e "s/^$release://" -e "s/:.*$//"`
-echo -n $release.$compat.$abi
+echo="echo"
+if test -x "/bin/echo" ; then
+	echo="/bin/echo"
+elif test -x "/usr/bin/echo" ; then
+	echo="/usr/bin/echo"
+fi	
+$echo -n $release.$compat.$abi
--- libsipwitch.pc.cmake
+++ libsipwitch.pc.cmake
@@ -1,6 +1,6 @@
 prefix=${CMAKE_INSTALL_PREFIX}
-libdir=${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}
-includedir=${CMAKE_INSTALL_PREFIX}/include
+libdir=${CMAKE_INSTALL_FULL_LIBDIR}
+includedir=${CMAKE_INSTALL_FULL_INCLUDEDIR}
 
 Name: libsipwitch
 Description: Sip Witch supporting library for plugins
